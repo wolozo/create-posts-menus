63cf65b0a03e5b3a1d20796f0e41866db43473c6
SherryBCreative <SherryBCreative@users.noreply.github.com>
2017-03-02 06:59:58 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Create Pages and Menus
+
+* Requires at least:  4.3
+* Tested up to:       4.7
+* Stable tag:         master
+* License:            GPLv2 or later
+* License URI:        http://www.gnu.org/licenses/gpl-2.0.html
+
+Add WordPress Posts and Menus with the function w_cpm_values()
+
+## Description
+
+```
+function w_cpm_values() {  
+  return array(
+
+    // Posts to create
+    //
+    // If 'slug' omitted, it will be generated via title.
+    //
+    // If slug exists, page will not be created.
+    'posts' => array(
+      array(
+        'title'   => 'Page 1',
+        'slug'    => 'page-1',
+        'content' => 'When these people drew near the house where Dorothy was standing in the doorway.',
+      ),
+
+      array(
+        'title'   => 'Page 2',
+        'content' => 'When these people drew near the house where Dorothy was standing in the doorway.',
+      ),
+
+      array(
+        'title'   => 'Page 3',
+        'content' => 'When these people drew near the house where Dorothy was standing in the doorway.',
+      ),
+
+    ),
+
+    // Menus and Menu Items
+    'menus' => array(
+      // 'Menu-Name' will be created if nonexistent.
+      'Menu-Name' => array(
+      
+        'delete-existing' => true,
+        
+        // Use 'post-slug' for pages/posts OR 'url' for Custom Links
+        // If 'post-slug' omitted, it will be generated via title unless 'url' is used.
+        array(
+          'title'      => 'Page 1',
+          'post-slug'  => 'page-1',       // optional
+          'classes'    => 'test-class',   // optional
+          'target'     => '_blank',       // optional
+          'attr_title' => 'Hello World',  // optional
+        ),
+        array(
+          'title'       => 'Page 2',
+          'url'         => 'http://www.google.com/',
+          'parent-slug' => 'page-1',
+        ),
+        array(
+          'title' => 'Page 3',
+        ),
+
+      ),
+    ),
+  );
+}
+```
+
+## Installation
+
+The easiest way to install and maintain this plugin is with [GitHub Updater](https://github.com/afragen/github-updater). The great thing about using GitHub Updater is that the plugin will then be able to be updated through the regular WordPress update process.
+
+Of course, if you wish to install and update the plugin manually, you are welcome to do so.
+
+### Installing with GitHub Updater
+1. First make sure you have [GitHub Updater](https://github.com/afragen/github-updater/wiki/Installation) itself installed.
+2. In the WordPress Dashboard go to *Settings > GitHub Updater > Install Plugin*.
+3. Enter `https://github.com/wolozo/create-posts-menus` as the Plugin URI and make sure the Remote Repository Host is `Github`. Then click the "Install Plugin".
+4. Activate the plugin after it has installed.
---
+++ create-posts-menus.php
@@ -0,0 +1,227 @@
+<?php
+
+/*
+ * Plugin Name:       00 - Create Posts and Menus
+ * Plugin URI:        https://github.com/wolozo/create-posts-menus
+ * GitHub Plugin URI: https://github.com/wolozo/create-posts-menus
+ * Description:       Create Posts and Menus within the theme code.
+ * Version:           0.0.1
+ * Author:            Wolozo
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       cpam
+ * Requires WP:       4.3
+ * Requires PHP:      5.3
+ */
+
+// @fixme w_cpam_create_menu_items() Custom Links are not verified and will be duplicated.
+// @todo Assign Menus To Theme Locations
+
+/**
+ * Make sure our global variables are set.
+ */
+function w_cpam_get_values() {
+  static $w_cpm_values;
+
+  if ( is_array( $w_cpm_values ) ) : return $w_cpm_values; endif;
+
+  if ( false === $w_cpm_values || ! function_exists( 'w_cpm_values' ) ) {
+    $w_cpm_values = false;
+
+    return false;
+  }
+
+  return $w_cpm_values = w_cpm_values();
+}
+
+add_action( 'init', 'w_cpam_get_values', 1 );
+
+/**
+ * Creates a navigation menu using $w_cpam_menuName
+ *
+ * @param $menuName
+ *
+ * @return bool
+ */
+function w_cpam_create_menu( $menuName ) {
+
+  if ( ! is_nav_menu( $menuName ) ) {
+    wp_create_nav_menu( $menuName );
+
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * Creates the navigation menu from w_cpm_values()
+ *
+ */
+function w_cpam_create_menus() {
+  if ( false === ( $w_cpm_values = w_cpam_get_values() ) && ! isset( $w_cpm_values[ 'menus' ] ) ) {
+    return;
+  }
+
+  foreach ( $w_cpm_values[ 'menus' ] as $menuName => $menu ) {
+
+    $is_nav_menu = is_nav_menu( $menuName );
+    if ( ! $is_nav_menu || ( $is_nav_menu && $menu[ 'delete-existing' ] ) ) {
+      wp_delete_nav_menu( $menuName );
+      wp_create_nav_menu( $menuName );
+    }
+
+    unset( $menu[ 'delete-existing' ] );
+
+    w_cpam_create_menu_items( $menuName, $menu );
+  }
+}
+
+add_action( 'init', 'w_cpam_create_menus', 30 );
+
+/**
+ * @param $menuName
+ * @param $menu
+ *
+ * @return bool
+ */
+function w_cpam_create_menu_items( $menuName, $menu ) {
+
+  if ( ! isset( $menuName ) || ! isset( $menu ) //
+       || false === $menuObject = wp_get_nav_menu_object( $menuName )
+  ) {
+    var_dump( 'something went wrong' );
+    exit;
+
+    return false;
+  }
+
+  $menuItems = wp_get_nav_menu_items( $menuName );
+
+  foreach ( $menu as $index => $menuItem ) {
+    if ( ! isset( $menuItem[ 'title' ] ) ) {
+      var_dump( 'title required' );
+      exit;
+    }
+
+    // Skip posts already within navigation menu
+    if ( ! array_key_exists( 'url', $menuItem ) ) {
+
+      if ( ! isset( $menuItem[ 'post-slug' ] ) ) {
+        $menuItem[ 'post-slug' ] = sanitize_title( $menuItem[ 'title' ] );
+      }
+
+      $menuItem_objectID = get_page_by_path( $menuItem[ 'post-slug' ] )->ID;
+
+      // Skip already created menu items by check of $menuItem_objectID exist within $menuItems as object_id
+      $chk = false;
+
+      foreach ( $menuItems as $index => $object ) {
+        if ( $chk = $menuItem_objectID == (string) $object->object_id ) : break; endif;
+      }
+
+      if ( $chk ) : continue; endif;
+    }
+
+    $menu_item_data = array(
+      'menu-item-title'  => $menuItem[ 'title' ],
+      'menu-item-status' => 'publish',
+    );
+
+    if ( isset( $menuItem[ 'parent-slug' ] ) ) {
+      $menuItems = wp_get_nav_menu_items( $menuName );
+
+      foreach ( $menuItems as $index => $object ) {
+
+        if ( get_page_by_path( $menuItem[ 'parent-slug' ] )->ID == (string) $object->object_id ) {
+          $menu_item_data[ 'menu-item-parent-id' ] = $object->ID;
+
+          break;
+        }
+      }
+    }
+
+    if ( isset( $menuItem[ 'url' ] ) ) {
+      $menu_item_data[ 'menu-item-url' ] = $menuItem[ 'url' ];
+    } else {
+      $menu_item_data[ 'menu-item-object' ]    = 'page';
+      $menu_item_data[ 'menu-item-object-id' ] = $menuItem_objectID;
+      $menu_item_data[ 'menu-item-type' ]      = 'post_type';
+    }
+
+    if ( isset( $menuItem[ 'position' ] ) ) {
+      $menu_item_data[ 'menu-item-position' ] = $menuItem[ 'position' ];
+    }
+
+    if ( isset( $menuItem[ 'classes' ] ) ) {
+      $menu_item_data[ 'menu-item-classes' ] = $menuItem[ 'classes' ];
+    }
+
+    if ( isset( $menuItem[ 'target' ] ) ) {
+      $menu_item_data[ 'menu-item-target' ] = $menuItem[ 'target' ];
+    }
+
+    if ( isset( $menuItem[ 'attr_title' ] ) ) {
+      $menu_item_data[ 'menu-item-attr-title' ] = $menuItem[ 'attr_title' ];
+    }
+
+    wp_update_nav_menu_item( $menuObject->term_id, 0, $menu_item_data );
+  }
+}
+
+//add_action( 'init', 'w_cpam_create_menu_items', 20 );
+
+/**
+ * Create pages on init from $w_cpam_pages
+ *
+ * @todo handle post_type
+ */
+function w_cpam_create_posts() {
+
+  if ( false === ( $w_cpm_values = w_cpam_get_values() ) && ! is_array( $w_cpm_values[ 'pages' ] ) ) {
+    return;
+  }
+
+  foreach ( $w_cpm_values[ 'pages' ] as $index => $newPage ) {
+
+    // add slug from title if empty.
+    if ( ! array_key_exists( 'slug', $newPage ) ) : $newPage[ 'slug' ] = sanitize_title( $newPage[ 'title' ] ); endif;
+
+    // bypass existing slugs
+    if ( ! w_cpam_slug_exist( $newPage[ 'slug' ] ) ) {
+
+      $postarr = array(
+        'post_title'     => $newPage[ 'title' ],
+        'post_type'      => 'page',
+        'post_name'      => $newPage[ 'slug' ],
+        'comment_status' => 'closed',
+        'ping_status'    => 'closed',
+        'post_content'   => $newPage[ 'content' ],
+        'post_status'    => 'publish',
+        'post_author'    => 1,
+        'menu_order'     => 0
+      );
+
+      // and create page
+      wp_insert_post( $postarr );
+    }
+  }
+}
+
+add_action( 'init', 'w_cpam_create_posts', 10 );
+
+/**
+ * Check if slug exists
+ *
+ * @param $slug
+ *
+ * @return bool
+ */
+function w_cpam_slug_exist( $slug ) {
+  global $wpdb;
+
+  if ( $wpdb->get_row( "SELECT post_name FROM wp_posts WHERE post_name = '" . $slug . "'", 'ARRAY_A' ) ) {
+    return true;
+  }
+
+  return false;
+}
